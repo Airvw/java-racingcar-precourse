7b4f24398604dca2554f96772efbb35f9c6834e5
DoubleK <airvw14@gmail.com>
2022-10-05 22:55:47 +0900

    feat: make RacingCar with name, position

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public class RacingCar {
+    private String racingCarName;
+    private int racingCarPosition;
+
+
+    public RacingCar(String racingCarName, int racingCarPosition) {
+        this.racingCarName = racingCarName;
+        this.racingCarPosition = racingCarPosition;
+    }
+
+    public String getRacingCarName() {
+        return racingCarName;
+    }
+
+    public int getRacingCarPosition() {
+        return racingCarPosition;
+    }
+}
