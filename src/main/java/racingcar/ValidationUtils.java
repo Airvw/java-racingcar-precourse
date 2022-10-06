b99d462a115663c1752f696532acb9f882bb6b2f
DoubleK <airvw14@gmail.com>
2022-10-06 23:57:49 +0900

    feat: add input validation methods
    new Methods
    - validate input carNames
    - validate input tryCnt
    - split string by regex

--- src/main/java/racingcar/ValidationUtils.java
+++ src/main/java/racingcar/ValidationUtils.java
@@ -2,29 +2,36 @@ package racingcar;
 
 public class ValidationUtils {
 
-    public static final String CAR_REGEX = ",";
+    private static final String CAR_REGEX = ",";
     private static final String CNT_REGEX = "[0-9]+";
-    public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+
+    public final int MIN_NUMBER = 0;
+    public final int MAX_NUMBER = 9;
 
     private static final int LIMIT_LENGTH = 5;
 
-    public boolean validCarNames(String[] carNames) {
+    public String[] validateCarNames(String[] carNames) throws IllegalArgumentException {
         for(String carName : carNames){
             isValidCarName(carName);
         }
-        return true;
+        return carNames;
     }
 
-    private void isValidCarName(String carName) {
+    private void isValidCarName(String carName) throws IllegalArgumentException {
         if(carName.length() > LIMIT_LENGTH) throw new IllegalArgumentException(ERROR_MESSAGE + LIMIT_LENGTH + "글자를 초과하여 입력하였습니다.");
     }
 
-    public int isValidCnt(String inputTryCnt) {
+    public int isValidCnt(String inputTryCnt) throws IllegalArgumentException {
         if(!inputTryCnt.matches(CNT_REGEX)) throw new IllegalArgumentException(ERROR_MESSAGE + "숫자 이외의 값을 입력하였습니다.");
         return Integer.parseInt(inputTryCnt);
     }
 
-    public static String[] makeSplit(String inputCarNames) {
+    public static String[] makeSplitedArray(String inputCarNames) {
         return inputCarNames.split(CAR_REGEX);
+    }
+
+    public boolean isValidNum(int result) {
+        return result >= MIN_NUMBER && result <= MAX_NUMBER;
     }
 }
