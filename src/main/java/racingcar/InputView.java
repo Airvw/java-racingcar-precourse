05b55406eb861f1a983452720d39acfd0466a232
DoubleK <airvw14@gmail.com>
2022-10-06 23:58:44 +0900

    feat: add input exceptions

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,16 +1,30 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.ValidationUtils.makeSplitedArray;
+
 
 public class InputView {
 
-    public String inputCarNames(){
+    ValidationUtils validationUtils = new ValidationUtils();
+
+    public String[] inputCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return readLine();
+        try {
+            return validationUtils.validateCarNames(makeSplitedArray(readLine()));
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputCarNames();
+        }
     }
 
-    public String inputTryCnt(){
+    public int inputTryCnt(){
         System.out.println("시도할 횟수는 몇회인가요?");
-        return readLine();
+        try {
+            return validationUtils.isValidCnt(readLine());
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputTryCnt();
+        }
     }
 }
