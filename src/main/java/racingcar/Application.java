15d6a636e75eec798b21af27b00bfe25e653bbad
DoubleK <airvw14@gmail.com>
2022-10-10 21:07:14 +0900

    feat: race game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,14 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        String[] carNames = inputView.inputCarNames();
+        int tryCnt = inputView.inputTryCnt();
+        RacingCars racingCars = new RacingCars(carNames);
+        while(tryCnt-- > 0){
+            racingCars.race();
+        }
+        resultView.printWinner(racingCars.getWinnerCar());
     }
 }
